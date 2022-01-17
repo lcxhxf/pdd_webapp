afdec17f98af225e5ca8e0386bf0464f5fe401a9
罗超 <2422635277@qq.com>
2022-01-17 15:34:03 +0800

    所有静态页面写完

--- src/pages/Talk/Talk.jsx
+++ src/pages/Talk/Talk.jsx
@@ -2,7 +2,9 @@ import React ,{memo}from 'react'
 import './Talk.css'
 import { connect } from 'react-redux' 
 import Scroll  from '../../baseUI/scroll/index'
-import img1 from '../../assets/images/1.jpg'
+import img1 from '../../assets/images/店铺评价.png'
+import img2 from '../../assets/images/分享2.png'
+import Goods from '../../components/goods/goods'
 
 const Talk = (props) =>{
    
@@ -15,8 +17,26 @@ const Talk = (props) =>{
                 refresh={false}
             >
                 <div className='talk'>
-                   <img src={img1} alt="" />
+                    <div className="talk-title">聊天</div>
+                   <div className="talk-shop">
+                       <div className="">
+                           <img src={img1} alt="" />
+                       </div>
+                       <div className="">
+                           <div className="talk-shop-t1">增肌超人店铺</div>
+                           <div className="talk-shop-t2">交易成功通知</div>
+                       </div>
+                       <div className="talk-shop-t3">22/01/17</div>
+                   </div>
+                   <div className="talk-text">
+                       <div className="">
+                           <img src={img2} alt="" />
+                       </div>
+                       <div className="talk-text-t1">为你推荐</div>
+                   </div>
+                   <Goods/>
                 </div>
+                
             </Scroll>
         </>
     )
