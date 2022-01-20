af49689b6c8a92497fbbae2d7e17b0cb45415260
罗超 <2422635277@qq.com>
2022-01-20 18:03:50 +0800

    购买页面

--- src/pages/GoodsDetail/GoodsDetail.jsx
+++ src/pages/GoodsDetail/GoodsDetail.jsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react'
+import React, { memo, useState,useEffect } from 'react'
 import { connect } from 'react-redux'
 import './GoodsDetail.css'
 import img1 from '../../assets/images/返回.png'
@@ -13,6 +13,19 @@ import img9 from '../../assets/images/商家.png'
 import img10 from '../../assets/images/空心.png'
 import img11 from '../../assets/images/消息.png'
 import Goods from '../../components/goods/goods'
+import img12 from '../../assets/images/close-l.png'
+import img13 from '../../assets/images/wxb定位.png'
+import img14 from '../../assets/images/箭头.png'
+import img15 from '../../assets/images/16-36-00.png'
+import img16 from '../../assets/images/加号.png'
+import img17 from '../../assets/images/减号.png'
+
+
+import Scroll from '../../baseUI/scroll/index'
+import {
+    ListContainer,
+} from "./GoodsDetail.style";
+
 
 
 import { useHistory } from 'react-router-dom'
@@ -22,9 +35,38 @@ const GoodsDetail = (props) => {
     const handleClick = () => {
         history.push('/home/main')
     }
+    let [buy,setBuy] = useState(false)
+    let [kindstyle,setKindstyle] = useState(0)
+    let [name,setName] = useState('20款M1/8G/256G灰N63')
+    let [addNum,setAddNum] = useState(1)
+    let [preprice,setPreprice] = useState(6120)
+    let [totalprice,setTotalprice] = useState(6120)
+    let kind = ['20款M1/8G/256G灰N63','20款M1/8G/256G银N93', '20款M11/8G/256G金ND3','20款M1/8G/512G灰N73','20款M1/8G/512G银NA3', 'M1芯片256定制16G银','M1芯片256定制16G金','M1芯片1TB定制16G银灰金', 'M1芯片256定制16G金','M1芯片1TB定制16G银灰金','M1芯片256定制16G金', 'M1芯片1TB定制16G银灰金','M1芯片256定制16G金','M1芯片1TB定制16G银灰金', 'M1芯片256定制16G金','M1芯片1TB定制16G银灰金']
+    const buyClick = () => {
+        setBuy(true)
+    }
+    const closeClick = () => {
+        setBuy(false)
+    }
+    const reduceClick = () => {
+        setAddNum(addNum-1)
+        setTotalprice(addNum*preprice)
+    }
+    const addClick = () => {
+        setAddNum(addNum+1)
+        setTotalprice(addNum*preprice)
+    }
+    const kindClick = (e) => {
+        // console.log(e);
+        setKindstyle(e.target.id)
+        setName(e.target.innerHTML)
+    }
+    // useEffect(() => {
+
+    // }, totalprice)
     return (
         <>
-            <div className="goodsdetail">
+            <div className="goodsdetail" style={buy?{}:{}}>
                 <div className="goodsdetail-back">
                     <div className="" onClick={() => { handleClick() }}><img src={img1} alt="" /></div>
                     <div className="goodsdetail-back-title">商品详情</div>
@@ -130,7 +172,7 @@ const GoodsDetail = (props) => {
                 <div className="title2">推荐</div>
                 <Goods />
             </div>
-            <div className="tabbar">
+            <div className="tabbar" onClick={() => { buyClick() }}>
                 <div className="tabbar1">
                     <div className=""><img src={img9} alt="" /></div>
                     <div className="">店铺</div>
@@ -152,6 +194,63 @@ const GoodsDetail = (props) => {
                     <div className=""> 发起拼单</div>
                 </div>
             </div>
+            <div className="buy" style={buy?{}:{display: "none"}}>
+                <div className="buy-head">
+                    <div className="buy-head-title">确认订单</div>
+                    <div className="">
+                        <img src={img12} alt="" onClick={() => { closeClick() }}/>
+                    </div>
+                </div>        
+                <Scroll
+                    direction="vertical"
+                    refresh={false}
+                    pullUpLoading={true}
+                    pullDownLoading={true}
+                >
+                    <div className="">
+                      <div className="buy-address">
+                          <div className="">
+                              <img src={img13} alt="" />
+                          </div>
+                          <div className="">罗*,178****3357,**省**市****I昌北开发区*****************3号楼2栋</div>
+                          <div className="">
+                              <img src={img14} alt="" />
+                          </div>
+                      </div>
+                      <div className="buy-img">
+                          <div className="">
+                              <img src={img15} alt="" />
+                          </div>
+                          <div className="">
+                              <div className="buy-img-price">￥{totalprice}</div>
+                              <div className="buy-img-name">已选: {name}</div>
+                              <div className="buy-img-num">
+                                  <div className="buy-img-add">
+                                      <img src={img17} alt="" onClick={() => { reduceClick() }}/>
+                                  </div>
+                                  <div className="buy-img-add-num">{addNum}</div>
+                                  <div className="">
+                                      <img src={img16} alt="" onClick={() => { addClick() }}/>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                      <div className="buy-kind">
+                          <p className='buy-kind-title'>颜色分类</p>
+                          {
+                            kind.map((item,index) => {
+                                return <div className="111" id={index} onClick={(e) => { kindClick(e) }} style={kindstyle == index?{"background-color": "red",color: "#fff"}:{}}>{item}</div>
+                            })
+                          }
+                          {/* <div className="" onClick={() => { kindClick() }} style={}>20款M1/8G/256G灰N63</div> */}  
+                      </div>
+                    </div>
+                </Scroll>
+                <div className="buy-buttom">
+                    立即支付￥{totalprice}
+                </div>
+            </div>
+            <div className="buy-hover" style={buy?{}:{display: "none"}}></div>
         </>
     )
 }
---
+++ src/pages/GoodsDetail/GoodsDetail.style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import style from '../../assets/global-style';
+
+export const ListContainer = styled.div `
+position: fixed;
+top: 70px;
+left: 0;
+bottom: 0;
+overflow: hidden;
+width: 100%;
+height: 540px;
+background-color: #f4f4f4;
+`;
